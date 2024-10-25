6135b7b08bd1b1d186aaeaafa762c4b22492142c
CasGade7 <cgj@specialisterne.com>
2024-10-25 11:13:17 +0200

    Create Opgave1.py

---
+++ Opgave1.py
@@ -0,0 +1,37 @@
+from functools import reduce
+
+names = [
+    "Alexander", "Benjamin", "Charlotte", "Daniel", "Emily", "Frederik", 
+    "Gabriel", "Hannah", "Isabella", "Jacob", "Katherine", "Liam", "Mia", 
+    "Nathan", "Olivia", "Peter", "Quinn", "Rebecca", "Samuel", "Theresa", 
+    "Ulysses", "Victoria", "William", "Xander", "Yasmine", "Zachary",
+    "Amelia", "Aaron", "Sophia", "Noah", "Ava", "James", "Lucas", "Ethan", 
+    "Ella", "David", "Elijah", "Aria", "Jackson", "Aiden", "Scarlett", 
+    "Sofia", "Matthew", "Logan", "Abigail", "Grace", "Henry", "Isla", 
+    "Ryan", "Evelyn", "Oliver", "Sebastian", "Harper", "Caleb", "Chloe", 
+    "Julian", "Penelope", "Levi", "Victoria", "Dylan", "Aurora", "Luke", 
+    "Hazel", "Isaac", "Samantha", "Theodore", "Lily", "Grayson", "Lillian", 
+    "Joshua", "Layla", "Zoe", "Madison", "Owen", "Caroline", "Leo", 
+    "Alice", "Mason", "Eleanor", "Wyatt", "Ellie", "Jack", "Nora", "Lucas",
+    "Sarah", "Evan", "Luna", "Mila", "Eli", "Sadie", "Landon", "Addison",
+    "Jaxon", "Piper", "Lincoln", "Stella", "Connor", "Grace", "Hudson", 
+    "Ruby", "Carson", "Sophia", "Asher", "Kinsley", "Christian", "Brielle",
+    "Maverick", "Vivian", "Nolan", "Emilia", "Hunter", "Camila", "Adrian", 
+    "Archer", "Easton", "Emery", "Maddox", "Faith", "Roman", "Riley"
+]
+for i in names:
+    print(i)
+    all_freq = dict(reduce(lambda x, y: {**x, y: x.get(y, 0) + 1}, i, {}))
+    print(all_freq)
+
+
+a = sorted(names, key=len)
+b = sorted(names)
+#print(a)
+#print(b)
+
+
+
+
+
+      
